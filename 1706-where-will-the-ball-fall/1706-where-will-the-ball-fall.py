74fc1018a01c63ff7c619bb4bc6564574d91819f
pratul2789 <pratul.trivedi2789@gmail.com>
2022-02-13 10:34:43 -0600

    Time: 189 ms (61.36%), Space: 14.2 MB (6.82%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def findBall(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[int]
+        """
+        if not grid: 
+            return []
+        
+        if len(grid[0]) == 1:
+            return [-1]
+        
+        
+        def canGoNext(i,j):
+            
+            if grid[i][j] == 1:
+                if j == len(grid[0]) -1:
+                    return False
+                
+                if grid[i][j+1] == -1:
+                    return False
+                return True
+            else:
+                if j == 0:
+                    return False
+                
+                if grid[i][j-1] == 1:
+                    return False
+                return True
+            
+        
+        
+        def dfs(row,i,j):
+            
+            if row == len(grid):
+                return j
+            
+            if canGoNext(i,j):
+                if grid[i][j] == 1:
+                    return dfs(row+1,i+1,j+1)
+                else:
+                    return dfs(row+1,i+1,j-1)
+            
+            return -1
+        
+        
+        res = []
+        
+        for i in range(len(grid[0])):
+            x = dfs(0,0,i)
+            if x != -1:
+                res.append(x)
+            else:
+                res.append(-1)
+                
+        return res
