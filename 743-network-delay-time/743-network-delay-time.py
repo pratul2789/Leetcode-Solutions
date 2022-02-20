9e7d1e9733055a8c1120156079ddb16d6b36d44e
pratul2789 <pratul.trivedi2789@gmail.com>
2022-02-20 17:35:52 -0600

    Time: 501 ms (63.60%), Space: 15.9 MB (45.40%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -19,17 +19,19 @@ class Solution(object):
         vis = set()
         
         heapq.heappush(heap,(0,k))
-        
+        #vis.add(k)
         ans = 0
         
         while heap:
             wt,node = heapq.heappop(heap)
-            
             if node not in vis:
+            #vis.add(node)
                 vis.add(node)
                 ans = max(ans,wt)
                 for nbr,wt2 in adj[node]:
-                    heapq.heappush(heap,(wt + wt2,nbr))
+                    if nbr not in vis:
+                        #vis.add(nbr)
+                        heapq.heappush(heap,(wt + wt2,nbr))
                     
         
         if len(vis) == n:
