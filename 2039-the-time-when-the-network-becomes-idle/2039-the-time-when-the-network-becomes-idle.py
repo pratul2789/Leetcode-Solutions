e036ed99a8084f6e6190338c3351c1972f13e2fb
pratul2789 <pratul.trivedi2789@gmail.com>
2022-02-21 11:48:16 -0600

    Time: 3902 ms (29.17%), Space: 70.2 MB (70.83%) - LeetHub

---
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.py
@@ -0,0 +1,57 @@
+from collections import defaultdict,deque
+class Solution(object):
+    def networkBecomesIdle(self, edges, patience):
+        """
+        :type edges: List[List[int]]
+        :type patience: List[int]
+        :rtype: int
+        """
+        
+        
+        adj = defaultdict(list)
+        shortestPath = defaultdict(int)
+        
+        for s,d in edges:
+            adj[s].append(d)
+            adj[d].append(s)
+        
+        def bfs():
+            q = deque()
+            vis = set()
+            vis.add(0)
+            q.append(0)
+            level = 0
+            while q:
+                l1 = len(q)
+                
+                for _ in range(l1):
+                    node = q.popleft()
+                    
+                    shortestPath[node] = level
+                    
+                    for nbr in adj[node]:
+                        if nbr not in vis:
+                            vis.add(nbr)
+                            q.append(nbr)         
+                level += 1
+        
+        bfs()
+        ans = 0
+        for node in range(1,len(patience)):
+            
+            pat = patience[node]
+            fullPath = (shortestPath[node] * 2)
+            
+            extraPackets = (fullPath - 1) // pat
+            
+            lastPacketSent = extraPackets * pat
+            
+            ans = max(ans,lastPacketSent + fullPath)
+            
+        return ans + 1
+            
+            
+        
+                
+                
+            
