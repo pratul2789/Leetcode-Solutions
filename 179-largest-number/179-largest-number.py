76d8236e0bdd97d1972749f1049456ec8bf550a8
pratul2789 <pratul.trivedi2789@gmail.com>
2022-02-22 18:01:32 -0600

    Time: 60 ms (30.05%), Space: 13.7 MB (13.38%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -4,17 +4,60 @@ class Solution(object):
         :type nums: List[int]
         :rtype: str
         """
+        if not nums:
+            return ""
         
         
-        for i in range(len(nums)):
-            nums[i] = str(nums[i])
-            
-        def comp(n1,n2):
-            if n1 + n2 > n2 + n1:
-                return -1
-            else:
-                return 1
-        nums = sorted(nums, key = cmp_to_key(comp))
-        if int(nums[0]) == 0:
+        def mergeSort(nums):
+            if len(nums) > 1:
+                
+                mid = len(nums) // 2
+                
+                l = nums[:mid]
+                
+                r = nums[mid:]
+                
+                mergeSort(l)
+                
+                mergeSort(r)
+                
+                i = 0
+                j = 0
+                k = 0
+                
+                while i < len(l) and j < len(r):
+                    n1 = l[i]
+                    n2 = r[j]
+                    
+                    n11 = str(n1) + str(n2)
+                    n21 = str(n2) + str(n1)
+                    
+                    if n11 > n21:
+                        nums[k] = l[i]
+                        i += 1
+                    else:
+                        nums[k] = r[j]
+                        j += 1
+                        
+                    k += 1
+                    
+                while i < len(l):
+                    nums[k] = l[i]
+                    i += 1
+                    k += 1
+                    
+                while j < len(r):
+                    nums[k] = r[j]
+                    j += 1
+                    k += 1
+                    
+        mergeSort(nums)
+        res = ""
+        if nums[0] == 0:
             return "0"
-        return "".join(nums)
+        for i in range(len(nums)):
+            res += str(nums[i])
+        
+        return res
+        #return "".join(nums)
+                    
