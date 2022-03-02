948eca13e181ee208b8cbbe7d577d73ed418422d
pratul2789 <pratul.trivedi2789@gmail.com>
2022-03-01 22:59:05 -0600

    Time: 592 ms (88.33%), Space: 15.7 MB (41.63%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+class Solution(object):
+    def canReorderDoubled(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
+        arr = sorted(arr)
+        
+        d = dict()
+        for i in arr:
+            if i not in d:
+                d[i] = 0
+            d[i] += 1
+            
+            
+        for i in arr:
+            if len(d) == 0:
+                return True
+            if i in d:
+                d[i] -= 1
+                if d[i] == 0:
+                    del d[i]
+            
+                if i < 0:
+                    if (i * -1) % 2 != 0:
+                        return False
+                    comp = i // 2
+                else:
+                    comp = i * 2
+
+                if comp not in d:
+                    return False
+
+                d[comp] -= 1
+                if d[comp] == 0:
+                    del d[comp]
+            
+        return False
+        
