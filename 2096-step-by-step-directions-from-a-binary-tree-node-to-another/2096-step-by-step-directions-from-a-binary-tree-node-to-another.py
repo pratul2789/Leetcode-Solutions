2d5bacb008f161278a75a7c069838ba9d90be44f
pratul2789 <pratul.trivedi2789@gmail.com>
2022-03-27 21:32:45 -0400

    Time: 940 ms (72.60%), Space: 57.4 MB (93.70%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,81 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        
+        def getLCA(root, startValue, destValue):
+            
+            if not root:
+                return None
+            
+            if root.val == startValue or root.val == destValue:
+                return root
+            
+            left = getLCA(root.left,startValue,destValue)
+            right = getLCA(root.right,startValue,destValue)
+            
+            if not left:
+                return right
+            
+            if not right:
+                return left
+            
+            return root
+        
+        
+        lcaNode = getLCA(root,startValue,destValue)
+        
+        if not lcaNode:
+            return ""
+        
+        
+        q = deque()
+        
+        pathToStart = ""
+        pathToDest = ""
+        
+        q.append((lcaNode,""))
+        
+        while q:
+            
+            node,pathSoFar = q.popleft()
+            
+            if node.val == startValue:
+                pathToStart = pathSoFar
+            
+            if node.val == destValue:
+                pathToDest = pathSoFar
+                
+            if node.left:
+                q.append((node.left,pathSoFar + "L"))
+            
+            if node.right:
+                q.append((node.right,pathSoFar + "R"))
+                
+        if root.val != startValue and root.val != destValue:
+            #Dono hi Valid hai
+            # Convert startNodePath ko into all U's
+            temp = ""
+            
+            for i in range(len(pathToStart)):
+                temp += "U"
+                
+            return temp + pathToDest
+        
+        #One of them is the root. Now we need to make sure that we return all U's if destination is the start node
+        
+        if root.val == destValue:
+            temp = ""
+            
+            for i in range(len(pathToStart)):
+                temp += "U"
+                
+            return temp
+        
+        return pathToDest
